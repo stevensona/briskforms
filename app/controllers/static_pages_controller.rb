1b40b6f0b3882f165bf84252656e44e2c3141423
Adam Stevenson <adamstevenson121@gmail.com>
2016-02-21 01:07:44 -0500

    Add terms of service to static controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,11 +5,6 @@ class StaticPagesController < ApplicationController
   end
   def guide
   end
-  def lets_encrypt
-    if params[:id] = ENV['LE_URL']
-      render text: ENV['LE_RESP'] 
-    else
-      render status: :not_found
-    end
+  def tos
   end
 end
