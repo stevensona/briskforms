de29c1c5b8f1a1cf3741aa54b13358bdf0e32a1d
adam stevenson <adamstevenson121@gmail.com>
2016-02-24 20:12:19 -0500

    typo in model validation

--- app/models/form.rb
+++ app/models/form.rb
@@ -3,8 +3,8 @@ class Form < ActiveRecord::Base
   after_validation :add_url_scheme
 
   def add_url_scheme
-    unless self.url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//]
-      self.url = "http://#{self.url}"
+    unless self.success_url[/\Ahttp:\/\//] || self.url[/\Ahttps:\/\//]
+      self.success_url = "http://#{self.url}"
     end
   end
 
