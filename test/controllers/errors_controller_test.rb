d8ba3c8a0321a0519397d2a39df31772dd97aee9
adam stevenson <adamstevenson121@gmail.com>
2016-02-24 19:16:49 -0500

    start reworking tests

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -3,17 +3,17 @@ require 'test_helper'
 class ErrorsControllerTest < ActionController::TestCase
   test "should get not_found" do
     get :not_found
-    assert_response :success
+    assert_response :not_found
   end
 
   test "should get forbidden" do
     get :forbidden
-    assert_response :success
+    assert_response :forbidden
   end
 
   test "should get error" do
     get :error
-    assert_response :success
+    assert_response :internal_server_error
   end
 
 end
